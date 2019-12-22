030a193ff9c078d9725ab66814d295118f6824a6
kayky lopes <kaykylopes@hotmail.com>
2019-12-22 19:11:02 -0300

    Adicionar arquivos de projeto.

---
+++ MeuAppVS/MeuMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using MeuAppVS;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeuAppVS
+{
+    public class MeuMiddleware
+    {
+
+        private readonly RequestDelegate _next;
+
+        public MeuMiddleware( RequestDelegate next)
+        {
+            _next = next;
+
+        }
+
+    public async Task InvokeAsync(HttpContext contexto)
+    {
+        Console.WriteLine("\n\r --------Antes--------\n\r");
+
+        await _next(contexto);
+
+        Console.WriteLine("\n\r --------Depois--------\n\r");
+    }
+}
+}
+
+public static class MeuMiddlewareExtension
+{
+    public static IApplicationBuilder UseMeuMiddleware(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<MeuMiddleware>();
+    }
+}
